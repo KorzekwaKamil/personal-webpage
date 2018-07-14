ac6cafa21bd109f7c328a11594e9b91662d53567
KorzekwaKamil <31800298+KorzekwaKamil@users.noreply.github.com>
2018-07-14 21:04:21 +1000

    Update open_field.js

--- scripts/open_field.js
+++ scripts/open_field.js
@@ -12,5 +12,5 @@ function openField(event, field,tab_color) {
                 tablinks[i].className = tablinks[i].className.replace(" w3-border-black", "");
             }
             document.getElementById(field).style.display = "block";
-            event.currentTarget.firstElementChild.className += tab_color;
+            event.currentTarget.className += tab_color;
 }
